bcdcf7d09220570c412e35cdc9c776a1e75b4883
Bartek <bartekwojtal1207@gmail.com>
2018-01-09 21:50:55 +0100

    example
    table bootstrap

--- index.php
+++ index.php
@@ -3,11 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <title>strona testowa dla knockout.js</title>
+
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"
             integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
             crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css"
+          integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"
+            integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+<!--    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/locale/bootstrap-table-zh-CN.min.js"></script>-->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>z
     <script type="text/javascript" src="assets/ko/index.js"></script>
     <script type="text/javascript" src="assets/index.js"></script>
+    <style>
+        .active{
+            color: red;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -24,8 +38,77 @@
         </ul>
     </div>
 
+<!--test dla table bootsrap-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="test-table">
+                <table data-search="true"
+                       id="mainTable">
+                    <thead>
+                    <tr>
+                        <th>Item ID</th>
+                        <th>Item Name</th>
+                        <th>Item Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Item 1</td>
+                        <td>$1</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Item 2</td>
+                        <td>$2</td>
+                    </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+        </div>
+    </div>
+    <form action="">
 
 
+        
+    </form>
+
+
+</div>
+<script type="text/javascript">
+var mainTable = $('#mainTable');
+var self = $(this);
+
+$(mainTable).on('click',function (e) {
+    // $(this).bootstrapTable('toggleView');
+});
+
+
+$(mainTable).bootstrapTable({
+    onClickRow: function (row, $element, field) {
+
+            console.log($element);
+
+            addTestClass($element);
+        deleteTest($element);
+    }
+});
+
+
+ function addTestClass ($element) {
+     $element.toggleClass('active')
+
+}
+function deleteTest($element){
+     console.log(mainTable);
+     self.mainTable.bootstrapTable('remove');
+}
+deleteTest(null)
+
+</script>
 
 </body>
 </html>
